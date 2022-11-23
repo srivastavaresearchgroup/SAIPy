dd2fcb280a297ee465d7240a3f37c121fa838929
Wei_Cyber <49800396+weicyber@users.noreply.github.com>
2022-11-23 13:21:38 +0100

    Create utils.py

---
+++ deepseis/utils/utils.py
@@ -0,0 +1,12 @@
+## visualization
+import matplotlib.pyplot as plt
+import numpy
+import torch
+import tensorflow as tf
+
+
+def creme_plot_func():
+  return True
+
+def dynapicker_plot_func():
+  return True
